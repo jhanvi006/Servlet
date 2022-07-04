1db040a944fc89b7f82726f4abcc23fbc241a43c
jhanvi006 <jhanvikankhara06@gmail.com>
2022-07-05 02:48:48 +0530

    Added: validation for user name added in servlet.

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -9,14 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
-        urlPatterns = { "/LoginServlet" },
-        initParams = {
-                @WebInitParam(name = "user", value = "jhanvi"),
-                @WebInitParam(name = "password", value = "jhanvi")
-        }
+        urlPatterns = { "/LoginServlet" }
+//        initParams = {
+//                @WebInitParam(name = "user", value = "jhanvi"),
+//                @WebInitParam(name = "password", value = "jhanvi")
+//        }
 )
 public class LoginServlet extends HttpServlet {
     @Override
@@ -27,15 +29,24 @@ public class LoginServlet extends HttpServlet {
         /* get servlet config params */
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)){
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+//        if (userId.equals(user) && password.equals(pwd)){
+        if (validateUser(user)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         }
         else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong.</font>");
+//            out.println("<font color=red>Either username or password is wrong.</font>");
+            out.println("<font color=red>Username is incorrect.</font>");
             rd.include(request, response);
         }
+    }
+    private boolean validateUser(String user) {
+        String regex = "^[A-Z][A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(user);
+        boolean result = matcher.matches();
+        return result;
     }
 }
